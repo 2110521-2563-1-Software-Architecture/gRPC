11a73b7cbaa18214a0b43c5d1a0b67539a2b46c9
N <n@n.n>
2020-09-03 22:57:20 +0700

    format grpc

--- gRPC/server.js
+++ gRPC/server.js
@@ -1,42 +1,26 @@
-/*
-   Copyright 2016, Google, Inc.
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-       http://www.apache.org/licenses/LICENSE-2.0
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-*/
-
 var grpc = require('grpc');
-
-var booksProto = grpc.load('books.proto');
-
+// add the events package
 var events = require('events');
+// add the bookStream global variable
 var bookStream = new events.EventEmitter();
-
+var booksProto = grpc.load('books.proto');
 // In-memory array of book objects
-var books = [{
-    id: 123,
-    title: 'A Tale of Two Cities',
-    author: 'Charles Dickens'
-}];
-
+var books = [
+    { id: 123, title: 'A Tale of Two Cities', author: 'Charles Dickens' }
+];
 var server = new grpc.Server();
 server.addService(booksProto.books.BookService.service, {
-    list: function(call, callback) {
+    list: function (call, callback) {
         callback(null, books);
     },
-    insert: function(call, callback) {
+    insert: function (call, callback) {
         var book = call.request;
         books.push(book);
+        // add the following to the insert method
         bookStream.emit('new_book', book);
         callback(null, {});
     },
-    get: function(call, callback) {
+    get: function (call, callback) {
         for (var i = 0; i < books.length; i++)
             if (books[i].id == call.request.id)
                 return callback(null, books[i]);
@@ -45,7 +29,7 @@ server.addService(booksProto.books.BookService.service, {
             details: 'Not found'
         });
     },
-    delete: function(call, callback) {
+    delete: function (call, callback) {
         for (var i = 0; i < books.length; i++) {
             if (books[i].id == call.request.id) {
                 books.splice(i, 1);
@@ -57,12 +41,12 @@ server.addService(booksProto.books.BookService.service, {
             details: 'Not found'
         });
     },
-    watch: function(stream) {
-        bookStream.on('new_book', function(book){
+    watch: function (stream) {
+        bookStream.on('new_book', function (book) {
             stream.write(book);
         });
-    }
+    },
 });
-
 server.bind('0.0.0.0:50051', grpc.ServerCredentials.createInsecure());
+console.log('Server running at http://0.0.0.0:50051');
 server.start();
