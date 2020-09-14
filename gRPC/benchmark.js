954206509b92a5ef15b2f82a4553a0658345c168
N <N@N.N>
2020-09-15 00:18:36 +0700

    scenario1.1 + scenario3

---
+++ gRPC/benchmark.js
@@ -0,0 +1,81 @@
+var grpc = require('grpc');
+var booksProto = grpc.load('books.proto');
+// var request = require('request');
+var moment = require('moment');
+const { without } = require('lodash');
+var client = new booksProto.books.BookService('0.0.0.0:50051', grpc.credentials.createInsecure());
+let responseTimeArr = [];
+function printResponse(error, response) {
+	if (error) console.log('Error: ', error);
+	else console.log(response);
+}
+async function listBooks() {
+	return new Promise((resolve) => {
+		client.list({}, function (error, books) {
+			//printResponse(error, books);
+			resolve(true);
+		});
+	});
+}
+async function insertBook(id, title, author) {
+	var book = {
+		id: parseInt(id),
+		title: title,
+		author: author,
+	};
+	return new Promise((resolve) => {
+		client.insert(book, function (error, books) {
+			printResponse(error, books);
+			resolve(true);
+		});
+	});
+}
+function getBook(id) {
+	var startDate = moment();
+	client.get(
+		{
+			id: parseInt(id),
+		},
+		function (error, book) {}
+	);
+}
+function deleteBook(id) {
+	var startDate = moment();
+	client.delete(
+		{
+			id: parseInt(id),
+		},
+		function (error, empty) {
+			var endDate = moment();
+			console.log('Request took: ' + endDate.diff(startDate) + ' ms.');
+			// printResponse(error, empty);
+			responseTimeArr.push(endDate.diff(startDate));
+		}
+	);
+}
+
+var scenario = process.argv[2];
+(async () => {
+	if (scenario == 's1') {
+		const start = moment();
+		await insertBook(process.argv[3], process.argv[4], process.argv[5]);
+		const end = moment();
+		console.log('Request took: ' + end.diff(start) + ' ms.');
+	} else if (scenario == 's2') {
+	} else if (scenario == 's3') {
+		const response = [];
+		for (let i = 1; i <= 3; i++) {
+			const start = moment();
+			const toWait = [];
+			for (let c = 1; c <= i; c++) {
+				toWait.push(listBooks());
+			}
+			await Promise.all(toWait);
+			const end = moment();
+			response.push(end.diff(start));
+		}
+		console.log(response);
+	} else {
+		listBooks();
+	}
+})();
--- gRPC/server.js
+++ gRPC/server.js
@@ -5,47 +5,52 @@ var events = require('events');
 var bookStream = new events.EventEmitter();
 var booksProto = grpc.load('books.proto');
 // In-memory array of book objects
-var books = [
-    { id: 123, title: 'A Tale of Two Cities', author: 'Charles Dickens' }
-];
+var books = [{ id: 123, title: 'A Tale of Two Cities', author: 'Charles Dickens' }];
 var server = new grpc.Server();
 server.addService(booksProto.books.BookService.service, {
-    list: function (call, callback) {
-        callback(null, books);
-    },
-    insert: function (call, callback) {
-        var book = call.request;
-        books.push(book);
-        // add the following to the insert method
-        bookStream.emit('new_book', book);
-        callback(null, {});
-    },
-    get: function (call, callback) {
-        for (var i = 0; i < books.length; i++)
-            if (books[i].id == call.request.id)
-                return callback(null, books[i]);
-        callback({
-            code: grpc.status.NOT_FOUND,
-            details: 'Not found'
-        });
-    },
-    delete: function (call, callback) {
-        for (var i = 0; i < books.length; i++) {
-            if (books[i].id == call.request.id) {
-                books.splice(i, 1);
-                return callback(null, {});
-            }
-        }
-        callback({
-            code: grpc.status.NOT_FOUND,
-            details: 'Not found'
-        });
-    },
-    watch: function (stream) {
-        bookStream.on('new_book', function (book) {
-            stream.write(book);
-        });
-    },
+	list: function (call, callback) {
+		callback(null, books);
+	},
+	insert: function (call, callback) {
+		var book = call.request;
+		books.push(book);
+		// add the following to the insert method
+		bookStream.emit('new_book', book);
+		callback(null, {});
+	},
+	insertList: function (call, callback) {
+		var bks = call.request;
+		for (let bk of bks) {
+			books.push(bk);
+			bookStream.emit('new_book', book);
+		}
+		callback(null, {});
+	},
+	get: function (call, callback) {
+		for (var i = 0; i < books.length; i++)
+			if (books[i].id == call.request.id) return callback(null, books[i]);
+		callback({
+			code: grpc.status.NOT_FOUND,
+			details: 'Not found',
+		});
+	},
+	delete: function (call, callback) {
+		for (var i = 0; i < books.length; i++) {
+			if (books[i].id == call.request.id) {
+				books.splice(i, 1);
+				return callback(null, {});
+			}
+		}
+		callback({
+			code: grpc.status.NOT_FOUND,
+			details: 'Not found',
+		});
+	},
+	watch: function (stream) {
+		bookStream.on('new_book', function (book) {
+			stream.write(book);
+		});
+	},
 });
 server.bind('0.0.0.0:50051', grpc.ServerCredentials.createInsecure());
 console.log('Server running at http://0.0.0.0:50051');
